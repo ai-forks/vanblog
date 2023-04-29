68badd8dca88c6d506924f2e4878cfff8b55cc3e
sd <leowest1921@gmail.com>
2023-04-29 10:18:39 +0800

    add

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ echo "Github: https://github.com/mereithhh/vanblog"
 echo "Version(Env): ${VAN_BLOG_VERSION}"
 echo "============================================="
 
+cd /app
 
 sed "s/VAN_BLOG_EMAIL/${EMAIL}/g" /app/caddyTemplate.json >/app/caddy.json
 caddy start --config /app/caddy.json
